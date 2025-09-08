5062c15ade29c4caa915d1773597abec8af14533
HerissonK <11513@holbertonstudents.com>
2025-09-08 10:07:46 +0200

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for value in row:
+            if value == row[0]:
+                print("{:d}".format(value), end="")
+            else:
+                print(" {:d}".format(value), end="")
+        print()
+    else:
+        print()
