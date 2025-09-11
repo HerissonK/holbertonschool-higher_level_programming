22951c29beecf8cf2cc4f42620d32c0c217fdc60
HerissonK <11513@holbertonstudents.com>
2025-09-11 14:44:16 +0200

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,18 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
-
-    convert = {
-        'M': 1000,
-        'D': 500,
-        'C': 100,
-        'L': 50,
-        'X': 10,
-        'V': 5,
-        'I': 1
-    }
+    convert = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     value = 0
-
     for i in range(len(roman_string)):
         if (
             i < len(roman_string) - 1
@@ -22,5 +12,4 @@ def roman_to_int(roman_string):
             value -= convert[roman_string[i]]
         else:
             value += convert[roman_string[i]]
-
     return value
