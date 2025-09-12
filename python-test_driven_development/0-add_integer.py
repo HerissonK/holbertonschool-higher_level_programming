a2426bbe5b38da1b4ec722791948c7196522826f
HerissonK <11513@holbertonstudents.com>
2025-09-12 13:45:04 +0200

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+"""Defines a function to add 2 integers."""
+
+
 def add_integer(a, b=98):
-    if type(a) not in [int, float]:
+    """Add 2 integers or floats cast as integers.
+
+    Args:
+        a: first number (int or float)
+        b: second number (int or float), default = 98
+
+    Returns:
+        int: the sum of a and b, casted as integers
+
+    Raises:
+        TypeError: if a or b are not int/float
+    """
+    if type(a) is bool or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) is bool or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return(int(a) + int(b))
+
+    return int(a) + int(b)
