b2ac9fb845a79a92f2af8b514bbb5532d3aaee93
HerissonK <11513@holbertonstudents.com>
2025-09-22 16:35:49 +0200

    task 3

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,21 @@
+# task_03_countediterator.py
+
+class CountedIterator:
+    def __init__(self, iterable):
+        """Initialise l'objet avec un itérable et un compteur."""
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        """Retourne le prochain élément de l'itérateur et incrémente le compteur."""
+        try:
+            item = next(self.iterator)  # Récupère l'élément suivant
+            self.count += 1             # Incrémente le compteur
+            return item
+        except StopIteration:
+            # Transmet l'exception si plus d'éléments
+            raise
+
+    def get_count(self):
+        """Retourne le nombre d'éléments déjà itérés."""
+        return self.count
