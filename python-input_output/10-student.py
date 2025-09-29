135c3b5d6fbe440ed4068527e1a476565fb13436
HerissonK <11513@holbertonstudents.com>
2025-09-29 19:53:12 +0200

    task 11 & 12

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,6 +11,14 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """returns dictionary descript for JSON serialization"""
-        return self.__dict__
+        if type(attrs) is list:
+            new_dict = {}
+            for key in self.__dict__:
+                for key2 in attrs:
+                    if key == key2:
+                        new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
