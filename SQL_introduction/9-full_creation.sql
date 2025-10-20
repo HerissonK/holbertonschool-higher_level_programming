8edd02e27283969b2908632226c07d0ead9a00a6
HerissonK <11513@holbertonstudents.com>
2025-10-20 12:13:13 +0200

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,7 @@
 --creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
