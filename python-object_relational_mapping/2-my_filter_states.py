967d87bbc07bba63f1af2346c2ab52bf35ad83fb
HerissonK <11513@holbertonstudents.com>
2025-10-21 20:54:01 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,15 +14,18 @@ if __name__ == '__main__':
     database = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Escape single quotes to handle edge cases
+    state_name_escaped = state_name.replace("'", "\\'")
+
     # Connect to MySQL server
     conn = MySQLdb.connect(host='localhost', port=3306,
                            user=username, passwd=password, db=database,
                            charset='utf8')
     cur = conn.cursor()
 
-    # Use = BINARY for case-sensitive exact match
+    # Case-sensitive exact match using BINARY
     query = ("SELECT * FROM states WHERE name = BINARY '{}' "
-             "ORDER BY id ASC".format(state_name))
+             "ORDER BY id ASC".format(state_name_escaped))
     cur.execute(query)
 
     rows = cur.fetchall()
