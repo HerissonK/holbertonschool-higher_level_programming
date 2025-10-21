c16d048d7288901654234ca9f3ed1609c5150bfa
HerissonK <11513@holbertonstudents.com>
2025-10-21 09:50:23 +0200

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS 'genre', COUNT(*) AS 'number_of_shows'
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_genres.id = genre_id
+GROUP BY genre_id
+ORDER BY number_of_shows DESC;
