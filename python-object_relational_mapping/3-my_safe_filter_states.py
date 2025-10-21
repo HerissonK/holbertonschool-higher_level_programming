6ed806fceb5516d89636f3868912438dacda3b28
HerissonK <11513@holbertonstudents.com>
2025-10-21 21:14:27 +0200

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+write a script that takes in arguments and displays
+all values in the states table of hbtn_0e_0_usa where
+name matches the argument. But this time, write one
+that is safe from MySQL injections!
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 5:
+        sys.exit('Use: 1-filter_states.py <mysql username> <mysql password>'
+                 ' <database name> <state name searched>')
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s "
+                "ORDER BY id ASC", (sys.argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
