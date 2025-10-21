c2c3cef6c0a0abda5b7f13b4ab2931af2f021d4c
HerissonK <11513@holbertonstudents.com>
2025-10-21 17:27:35 +0200

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
+import sys
+import MySQLdb
 
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
-                 ' <database name>')
+        sys.exit("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
 
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        # Connexion à la base MySQL
+        conn = MySQLdb.connect(host="localhost", port=3306,
+                               user=username, passwd=password,
+                               db=db_name, charset="utf8")
+
+        # Création du curseur
+        cur = conn.cursor()
+
+        # Exécution de la requête pour récupérer tous les états
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Affichage des résultats
+        for row in cur.fetchall():
+            print(row)
+
+        # Fermeture du curseur et de la connexion
+        cur.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
