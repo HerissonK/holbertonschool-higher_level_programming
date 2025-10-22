58ff6bf6d2b935eb9339d980e10afe10b0928928
HerissonK <11513@holbertonstudents.com>
2025-10-22 11:04:31 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,31 +6,30 @@ matches the argument.
 """
 
 
-import sys
+from sys import argv
 import MySQLdb
 
 
 if __name__ == '__main__':
     # Connect to MySQL server running on localhost at port 3306
     db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
 
     cursor = db.cursor()
 
     # Use format to create the SQL query with the user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
-    )
-    cursor.execute(query)
+    cursor.execute("SELECT * \
+                    FROM `states` \
+                    WHERE BINARY `name` = '{}' \
+                    ORDER BY id".format(argv[4]))
 
-    # Display results
     for state in cursor.fetchall():
         print(state)
 
-    cursor.close()
-    db.close()
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
